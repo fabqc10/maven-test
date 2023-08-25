bd70848207066c2ec8d138dbc5e2c082a1ee0291
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2023-08-25 18:52:29 +0200

    changing Calculator to new directory and edit the methods

---
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+public class Calculator {
+    public static void addition(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public static void substract(int a, int b){
+        System.out.println(a-b);
+    }
+
+    public static void divide(int a, int b){
+        System.out.println(a / b);
+    }
+
+    public static void multiply(int a, int b){
+        System.out.println(a * b);
+    }
+}
